7e2ceffb4fe6d3ed430d1f5a20ee4441bbb5bfe3
parth dave <parthdave>
2020-08-09 07:40:13 -0700

    Updates

---
+++ precache-manifest.055e50ef759c3f7b582b70ce44647831.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98b0acc8f4d3ec1730b7d5b1a0a12a0",
+    "url": "/reactapp1/index.html"
+  },
+  {
+    "revision": "f8a8754ecb85bbf429fc",
+    "url": "/reactapp1/static/js/2.8078e59a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp1/static/js/2.8078e59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b08df666e3791b315c",
+    "url": "/reactapp1/static/js/main.12997dd3.chunk.js"
+  },
+  {
+    "revision": "994a26a4b30a8645b9a8",
+    "url": "/reactapp1/static/js/runtime-main.5e6646e8.js"
+  }
+]);
